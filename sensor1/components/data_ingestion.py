dec16e57c88ea69798b80a1fabf2c575961f071a
Sanjoy <sanjoymlp2014@gmail.com>
2023-12-24 11:28:50 +0530

    project completed, first need run train.py then run main.py

--- sensor1/components/data_ingestion.py
+++ sensor1/components/data_ingestion.py
@@ -45,8 +45,8 @@ class DataIngestion:
             #split dataset into train and test set
             train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=42)
             
-            logging.info("create dataset directory folder if not available")
-            #create dataset directory folder if not available
+            logging.info("create dataset directory folderl if not available")
+            #create dataset directory folder if not avaiable
             dataset_dir = os.path.dirname(self.data_ingestion_config.train_file_path)
             os.makedirs(dataset_dir,exist_ok=True)
 
